1c78f16022939aabbc67155a22b463475be50c54
SpeakSoftly <speaksoftly@SpeakSofylt.local>
2020-04-30 21:52:53 +0800

    修复windows下浏览器vw, vh的bug

--- src/components/common/background/Background.vue
+++ src/components/common/background/Background.vue
@@ -1,13 +1,22 @@
 <template>
-  <vue-particles color="#dedede" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4"
-    linesColor="#dedede" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3"
-    :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push">
-  </vue-particles>
+  <vue-particles :style="{ height: bgHeight }" color="#dedede" :particleOpacity="0.7" :particlesNumber="80"
+    shapeType="circle" :particleSize="4" linesColor="#dedede" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4"
+    :linesDistance="150" :moveSpeed="3" :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push" />
 </template>
 
 <script>
   export default {
-    name: "Background"
+    name: "Background",
+    data() {
+      return {
+        bgHeight: window.innerHeight + "px"
+      }
+    },
+    created() {
+      window.onresize = () => {
+        this.bgHeight = window.innerHeight + "px"
+      }
+    }
   }
 </script>
 
